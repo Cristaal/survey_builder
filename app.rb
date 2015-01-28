13980c1c3ba409ee50d2c12560e79edc92050c99
Crystal Contreras-Grossman & monty rohde <student@epicodus.com>
2015-01-28 11:20:04 -0800

    add create ability for new surveys

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require("bundler/setup")
 Bundler.require(:default)
+require("./lib/survey")
+require("./lib/question")
 
 get('/') do
   @surveys = Survey.all()
@@ -7,4 +9,13 @@ get('/') do
 end
 
 post('/survey') do
+  title = params['title']
+  @survey = Survey.create({ :title => title})
+  @surveys = Survey.all()
+  erb(:index)
+end
+
+get('/survey/:id') do
+  @survey = Survey.find(params.fetch("id"))
+  erb(:create)
 end
